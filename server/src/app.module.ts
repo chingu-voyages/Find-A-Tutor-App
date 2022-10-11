30bfd02b89837868128b162ca05454090bee43dc
RZhang <richardzhiyuanzhang@gmail.com>
2022-10-11 19:16:08 -0400

    add dotenv, and update app.module to use dotenv variables. update root gitignore.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,17 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+// set up a .env file in server folder with the following env variables
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = Number(process.env.DB_PASSWORD);
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
+      host: DB_HOST,
+      port: DB_PORT,
       // your username
-      username: 'root',
+      username: DB_USERNAME,
       // your password
-      password: 'MySQL',
+      password: DB_PASSWORD,
       database: 'test',
       entities: [],
       // set to false during production
