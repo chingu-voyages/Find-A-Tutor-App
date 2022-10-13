6c4adacf52bf45bfea56fcdcaeccd9b6a95523e8
riboney <14286113+riboney@users.noreply.github.com>
2022-10-13 08:19:57 -0400

    feat: add configmodule to handle env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,32 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-// set up a .env file in server folder with the following env variables
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = Number(process.env.DB_PASSWORD);
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: DB_HOST,
-      port: DB_PORT,
-      // your username
-      username: DB_USERNAME,
-      // your password
-      password: DB_PASSWORD,
-      database: 'test',
-      entities: [],
-      // set to false during production
-      synchronize: true,
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
