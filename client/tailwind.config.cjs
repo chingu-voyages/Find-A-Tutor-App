4ff971b1b6a47e714091770a2ca931b9f32d1acb
riboney <14286113+riboney@users.noreply.github.com>
2022-10-16 16:25:09 -0400

    chore: setup daisyui and figma color pallete/font

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Inter', 'sans-serif'],
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#2E2E40',
+          secondary: '#93DAE5',
+          accent: '#F2F2F2',
+          neutral: '#FFFFFF',
+          'base-100': '#FFFFFF',
+        },
+      },
+      'dark',
+    ],
+  },
 };
