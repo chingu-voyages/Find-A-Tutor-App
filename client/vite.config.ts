d7b40e09becaba4f32bec2d1fe0e6f44f9c2e551
riboney <14286113+riboney@users.noreply.github.com>
2022-10-18 10:01:09 -0400

    feat: add proxy to server in dev environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: 'http://localhost:8080/',
+        changeOrigin: false,
+        secure: false,
+      },
+    },
+  },
+});
