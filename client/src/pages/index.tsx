a903b558a3bfc95073208d258f7d9d407dad5058
riboney <14286113+riboney@users.noreply.github.com>
2022-10-19 10:25:34 -0400

    refactor: add background color

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,6 @@
 function Index() {
   return (
-    <div className="w-screen h-full flex flex-col justify-center items-center">
+    <div className="w-screen h-full flex flex-col justify-center items-center bg-secondary">
       <h1 className="prose">Hello World!</h1>
       <button className="btn btn-primary text-neutral w-fit">
         I am a button
