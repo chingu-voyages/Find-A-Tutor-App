e4f5c4e1b21bc038b639f1c9d91e9d9535f8ff31
RZhang <richardzhiyuanzhang@gmail.com>
2022-10-20 19:21:07 -0400

    setup swagger at root db route

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const PORT = 8080;
@@ -6,6 +7,16 @@ const PORT = 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('bears-db')
+    .setDescription('Route testing and visual interface')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
