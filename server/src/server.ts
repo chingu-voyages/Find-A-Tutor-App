b2986d9eb9d3e740abf7e0411c9eb341cc293490
joeneedum <joeneedum@gmail.com>
2022-10-21 02:01:20 -0400

    create-server-file

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Hapi from '@hapi/hapi'
+
+const server: Hapi.Server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || '0.0.0.0',
+})
+
+export async function startServer(server: Hapi.Server): Promise<Hapi.Server> {
+    await server.start()
+    server.log('info', `Server running on ${server.info.uri}`)
+    return server
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+})
