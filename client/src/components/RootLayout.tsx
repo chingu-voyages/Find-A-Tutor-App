2584abb6349b3a02e7d1e2f874244b506f57d25a
riboney <14286113+riboney@users.noreply.github.com>
2022-10-27 20:15:41 -0400

    refactor: add short breakpoint to toggle h-full to fix content being cut off

--- client/src/components/RootLayout.tsx
+++ client/src/components/RootLayout.tsx
@@ -6,7 +6,7 @@ const RootLayout = () => {
   return (
     <div className="flex flex-col h-full justify-between">
       <Navbar />
-      <main className="w-screen h-full flex flex-col justify-center items-center bg-secondary">
+      <main className="w-screen h-full short:h-auto py-10 flex flex-col justify-center items-center bg-secondary">
         <Outlet />
       </main>
       <Footer />
