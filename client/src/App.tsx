7207d37d0defddb775fe8cd037eb0357b9b5b2c3
Liban <105807279+LibanBehi@users.noreply.github.com>
2022-10-31 19:28:10 -0400

    Ticket 58 59 (#67)
    * added error page
    
    * added error page
    
    * deleted error-page
    
    * added error page
    
    * Removed the .idea folder
    
    * Delete ErrorPage.tsx
    
    Deleted Empty file
    
    * Added error-page to routing and fixed linting for controller
    
    Co-authored-by: Ticia <84279128+TiciaD@users.noreply.github.com>
    Co-authored-by: Ticia Dunn <dunnticia633@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,11 @@ import {
 import Index from './pages/index';
 import Register from './pages/Register';
 import RootLayout from './components/RootLayout';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" errorElement={<ErrorPage />} element={<RootLayout />}>
       <Route index element={<Index />} />
       <Route path="account/register" element={<Register />} />
     </Route>,
