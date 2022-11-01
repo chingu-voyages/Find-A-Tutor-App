d755c090c1250459e10700d2e47d0cd63f95a766
Ticia Dunn <dunnticia633@gmail.com>
2022-11-01 02:50:09 +0000

    39 Tutor Profile UI     - Setup Profile page which will render TutorProfile or (when implemented) StudentProfile     - Create TutorProfile component to display tutor information     - Filler isAuthenticated to show extra sections when authenticated user views the component     - Filler fakeTutor object to demonstrate how Profile component receives data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,11 +8,13 @@ import Index from './pages/index';
 import Register from './pages/Register';
 import RootLayout from './components/RootLayout';
 import ErrorPage from './pages/ErrorPage';
+import Profile from './pages/Profile';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" errorElement={<ErrorPage />} element={<RootLayout />}>
       <Route index element={<Index />} />
+      <Route path="account" element={<Profile />} />
       <Route path="account/register" element={<Register />} />
     </Route>,
   ),
