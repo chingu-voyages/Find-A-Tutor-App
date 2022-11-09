bf306304ae092e1eda0ee8090e778aa0ed8c6e79
riboney <14286113+riboney@users.noreply.github.com>
2022-11-09 08:59:00 -0500

    feat: connect login page to backend API

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
 import { Formik, Form, FormikHelpers } from 'formik';
 import TextInput from '../components/common/TextInput';
 import Button from '../components/Button';
 import { LoginSchema } from '../utils/schemas';
+import { UseAppDispatch } from '../app/store';
+import { useNavigate } from 'react-router-dom';
+import { ILogin } from '../models/ILogin';
+import { login } from '../features/user.thunk';
 export interface LoginFormValues {
   email: string;
   password: string;
 }
 
 function Login() {
+  const dispatch = UseAppDispatch();
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="card md:w-96 bg-base-100 shadow-xl mx-3">
@@ -22,10 +28,19 @@ function Login() {
             values: LoginFormValues,
             { setSubmitting }: FormikHelpers<LoginFormValues>,
           ) => {
+            const newLogin: ILogin = {
+              email: values.email,
+              password: values.password,
+            };
+
+            dispatch(login(newLogin));
+
             setTimeout(() => {
               alert(JSON.stringify(values, null, 2));
               setSubmitting(false);
             }, 500);
+
+            navigate('/');
           }}
         >
           <Form className="card-body items-start gap-4">
