0e81b002dcf740cf0fef56d387f8c719a45a489d
riboney <14286113+riboney@users.noreply.github.com>
2022-11-09 08:56:34 -0500

    feat: add onClick prop to Button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,14 +1,16 @@
 type Props = {
   text: string;
   custom?: string[];
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 };
 
-const Button = ({ text, custom }: Props) => {
+const Button = ({ text, custom, onClick }: Props) => {
   const classes = custom && custom.join(' ');
 
   return (
     <button
       className={`btn btn-primary normal-case text-neutral px-6 py-2 text-md w-full lg:text-lg lg:w-auto lg:py-1 min-h-min h-min ${classes}`}
+      onClick={onClick}
     >
       {text}
     </button>
