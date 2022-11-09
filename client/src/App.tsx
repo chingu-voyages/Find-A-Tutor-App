889c220b12bb4a77606f938278daaf9711d72f22
riboney <14286113+riboney@users.noreply.github.com>
2022-11-09 08:57:22 -0500

    feat: show logout button and id when user logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,41 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { RootState } from './app/store';
+import { selectUser } from './features/user.slice';
 import Index from './pages/index';
 import Register from './pages/Register';
 import RootLayout from './components/RootLayout';
 import ErrorPage from './pages/ErrorPage';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
+import Home from './pages/Home';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" errorElement={<ErrorPage />} element={<RootLayout />}>
-      <Route index element={<Index />} />
-      <Route path="account" element={<Profile />} />
-      <Route path="account/register" element={<Register />} />
-      <Route path="account/login" element={<Login />} />
-    </Route>,
-  ),
-);
+const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 function App() {
+  const user = useTypedSelector(selectUser);
+
+  const homePage = () => {
+    return user ? (
+      <Route index element={<Home />} />
+    ) : (
+      <Route index element={<Index />} />
+    );
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" errorElement={<ErrorPage />} element={<RootLayout />}>
+        {homePage()}
+        <Route path="account" element={<Profile />} />
+        <Route path="account/register" element={<Register />} />
+        <Route path="account/login" element={<Login />} />
+      </Route>,
+    ),
+  );
+
   return <RouterProvider router={router} />;
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { selectUser } from '../features/user.slice';
+
+const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 function Home() {
-  return <h1>Home Page</h1>;
+  const user = useTypedSelector(selectUser);
+
+  console.log('user home', user);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {user && <p>Hello {user.id}</p>}
+    </div>
+  );
 }
 
 export default Home;
