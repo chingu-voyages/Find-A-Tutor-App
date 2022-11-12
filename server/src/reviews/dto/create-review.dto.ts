9dbd7e9db2016d8dc7d36eabc79928f975bfdc85
RZhang <richardzhiyuanzhang@gmail.com>
2022-11-11 22:36:49 -0500

    create reviews/post service and create-review dto.

--- server/src/reviews/dto/create-review.dto.ts
+++ server/src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,6 @@
-export class CreateReviewDto {}
+export class CreateReviewDto {
+  text: string;
+  rating: number;
+  profileId: number;
+  userId: number;
+}
--- server/src/reviews/reviews.service.ts
+++ server/src/reviews/reviews.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { PrismaService } from './../prisma/prisma.service';
 
 @Injectable()
 export class ReviewsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+    return this.prisma.review.create({
+      data: {
+        text: createReviewDto.text,
+        rating: createReviewDto.rating,
+        profile: {
+          connect: { id: createReviewDto.profileId },
+        },
+        user: {
+          connect: { id: createReviewDto.userId },
+        },
+      },
+    });
   }
 
   findAll() {
